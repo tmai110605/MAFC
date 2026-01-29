85a6cbc9c705a4a156c90c6407f3ae335d9c0144
_mqt. <23133071@student.hcmute.edu.vn>
2026-01-29 16:02:02 +0700

    Update mobilenet_MAFC.py

--- model/mobilenet_MAFC.py
+++ model/mobilenet_MAFC.py
@@ -20,7 +20,7 @@ class DepthwiseSeparableConvBlock(torch.nn.Module):
         self.use_res_skip = (in_channels == out_channels) and (stride == 1)
         self.conv_dw = conv3x3_dw_block(channels=in_channels, stride=stride)
         self.conv_pw = conv1x1_block(in_channels=in_channels, out_channels=out_channels)
-        self.cbamm = MAFC(out_channels, 16, pool_types=pool_types)
+        self.cbamm = BAM(out_channels, 16, pool_types=pool_types)
     def forward(self, x):
         if self.use_res_skip:
             residual = x
