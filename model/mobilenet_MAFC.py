24b874cb426e41d1194acd6e76b9222b7e99116c
_mqt. <23133071@student.hcmute.edu.vn>
2026-01-29 16:04:23 +0700

    Update mobilenet_MAFC.py

--- model/mobilenet_MAFC.py
+++ model/mobilenet_MAFC.py
@@ -253,7 +253,7 @@ class MobileNetV3(torch.nn.Module):
         self.backbone.add_module("final_conv1", conv1x1_block(in_channels=in_channels, out_channels=final_conv_channels[0], activation="hswish"))
         in_channels = final_conv_channels[0]
         if final_conv_se:            
-            self.backbone.add_module("final_se", MAFC(in_channels, 16, pool_types=pool_types))   
+            self.backbone.add_module("final_se", BAM(in_channels, 16, pool_types=pool_types))   
         self.backbone.add_module("final_pool", torch.nn.AdaptiveAvgPool2d(output_size=1))
         if len(final_conv_channels) > 1:
             self.backbone.add_module("final_conv2", conv1x1_block(in_channels=in_channels, out_channels=final_conv_channels[1], activation="hswish", use_bn=False))
