3f818a8e90278d74d5e6d493b17e7b938379335f
_mqt. <23133071@student.hcmute.edu.vn>
2026-02-18 20:15:32 +0700

    Update MAFAttention_main.py

--- MAFAttention_main.py
+++ MAFAttention_main.py
@@ -61,7 +61,7 @@ def get_args():
     #parser.add_argument('-r', '--data-root', type=str, required=True, help='Dataset root path.')
     parser.add_argument('-r', '--data-root', type=str, default='model', help='Dataset root path.')
     #parser.add_argument('-d', '--dataset', choices=['cifar10', 'cifar100', 'dogs'], required=True, help='Dataset name.')
-    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='dogs', help='Dataset name.')
+    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='cifar100', help='Dataset name.')
     parser.add_argument('--download', action='store_true', help='Download the specified dataset before running the training.')
     #parser.add_argument('-a', '--architecture', type=str, required=True, help='Model architecture name.')
     parser.add_argument('-a', '--architecture', type=str, default='mobilenetv1_w1', help='Model architecture name.')
@@ -215,7 +215,7 @@ def main():
         if not os.path.exists(pathout):
             os.makedirs(pathout)
         # get model
-        model = build_mobilenet_v1(120, width_multiplier=1.0, cifar=False,pool_types=['avg', 'std'])
+        model = build_mobilenet_v1(100, width_multiplier=1.0, cifar=True,pool_types=['avg', 'std'])
         model = model.to(device)
 
         print(model)
